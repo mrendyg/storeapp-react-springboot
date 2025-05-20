fbcd1d1887a860e1b6f7ba7f54ae407b43e935d9
andy <andygarcia.oy@gmail.com>
2025-05-19 22:04:38 -0400

    commit from my laptop

--- storeapp_react/src/App.tsx
+++ storeapp_react/src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
 
   return (
-    <p>Holaaa</p>
+    <p className='text-8xl'>Holaaa</p>
   );
 }
 
--- storeapp_react/tailwind.config.js
+++ storeapp_react/tailwind.config.js
@@ -1,11 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-<<<<<<< HEAD
 export default {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
-=======
-module.exports = {
-  content: ["./src/**/*.{html,js,tsx}"],
->>>>>>> 7ac49ea3ebeda6c0fc10579a93e2a783627a627e
   theme: {
     extend: {},
   },
